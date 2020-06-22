f22167a67e7b032d601290e1e0e9987718800368
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-06-21 22:23:20 -0500

    restart, card table format began

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Directory from "./pages/Directory";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Wrapper from "./components/Wrapper/Wrapper";
+import React from 'react';
+import PageLayout from '../src/components/pageLayout/PageLayout';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Directory} />
-          <Route exact path="/directory" component={Directory} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <PageLayout />
+    </div>
   );
 }
 
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,43 +1,7 @@
-import React from 'react';
+import axios from "axios";
 
-import axios from 'axios';
-
-export default class Employees extends React.Component {
-  state = {
-    employees: []
+export default {
+  getUsers: function() {
+    return axios.get("https://randomuser.me/");
   }
-
-  getRandomEmployees = function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random").then(res => {
-            const employees = res.data;
-            this.setState({ employees });
-          });
-  }
-  getEmployeeByName = function(employee) {
-    return axios.get("https://dog.ceo/api/breed/" + Employees + "/images").then(res => {
-      const employees = res.data;
-      this.setState({ employees });
-    });;
-  }
-  getEmployeesList = function() {
-    return axios.get("https://dog.ceo/api/breeds/list").then(res => {
-      const employees = res.data;
-      this.setState({ employees });
-    });;
-  }
-  // componentDidMount() {
-  //   axios.get(`https://jsonplaceholder.typicode.com/users`)
-  //     .then(res => {
-  //       const persons = res.data;
-  //       this.setState({ persons });
-  //     })
-  // }
-
-  // render() {
-  //   return (
-  //     <ul>
-  //       { this.state.persons.map(person => <li>{person.name}</li>)}
-  //     </ul>
-  //   )
-  // }
-}
+};
