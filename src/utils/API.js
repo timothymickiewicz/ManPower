cc937dc7bfc1d44798aeb499ebdd4a690afa9635
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-06-23 19:11:02 -0500

    cors config

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    }
+};
+
 export default {
   getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=20");
+    return axios.get("https://randomuser.me/api/?results=20", config)
   }
 };
